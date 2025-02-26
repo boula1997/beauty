52c9d339c122eddbad7fe40d4651056868b10756
Boula <boula.nessom.soliman@gmail.com>
2025-02-26 12:17:40 +0200

    commit

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -61,7 +61,7 @@ class PageController extends Controller
     {
 
         try {
-            $page = $this->page->create($request->except('images'));
+            $page = $this->page->create($request->except('images','profile_avatar_remove'));
             $page->uploadFiles();
             return redirect()->route('pages.index')
                 ->with('success', trans('general.created_successfully'));
