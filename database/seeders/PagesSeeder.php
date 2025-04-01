bb16c5a287cecbb3fe69c9e9890c41faae7d7468
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-04-01 11:22:45 +0200

    update

--- database/seeders/PagesSeeder.php
+++ database/seeders/PagesSeeder.php
@@ -76,7 +76,8 @@ class PagesSeeder extends Seeder
 
 
             for ($i = 0; $i < count($title_ar); $i++) 
-            { $page=Page::create([ 'ar'=> [
+            { 
+                $page=Page::updateOrCreate([ 'ar'=> [
                 'title' => $title_ar[$i],
                 'description' => $description_ar[$i],
                 'subtitle' => $subtitle_ar[$i],
