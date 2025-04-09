b4a48f0c1c8000e9993d3f201d956dab691a1411
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-04-09 14:36:45 +0200

    update

--- app/Http/Controllers/API/AboutController.php
+++ app/Http/Controllers/API/AboutController.php
@@ -3,69 +3,27 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\BenefitResource;
-use App\Http\Resources\CounterResource;
-use App\Http\Resources\CountryResource;
-use App\Http\Resources\FaqResource;
-use App\Http\Resources\FeatureResource;
+
 use App\Http\Resources\PageResource;
-use App\Http\Resources\PartnerResource;
-use App\Http\Resources\ProjectResource;
-use App\Http\Resources\ServiceResource;
-use App\Http\Resources\SliderResource;
-use App\Http\Resources\TeamResource;
-use App\Http\Resources\TestimonialResource;
-use App\Models\Counter;
-use App\Models\Feature;
-use App\Models\Partner;
-use App\Models\Service;
-use App\Models\Slider;
-use App\Models\Team;
-use App\Models\Country;
-use App\Models\Faq;
-use App\Models\Testimonial;
-use App\Models\Benefit;
-use App\Models\Project;
+
 use Exception;
-use Illuminate\Http\Request;
-use Khsing\World\World;
+
 
 
 class AboutController extends Controller
 {
-    private $slider;
-    private $counter;
-    private $country;
 
-    public function __construct(Partner $partner,Slider $slider,Counter $counter,Country $country)
+    public function __construct()
     {
-        $this->counter = $counter;
-        $this->slider = $slider;
-        $this->country = $country;
+
 
     }
     public function index()
     {
         try {
-            // $china = Country::getByCode('cn');
-            // $china->has_division; // true, otherwise is false
-            // $regsions = $china->children();Lebanon
-            // foreach(World::Countries() as $key=>$value)
-            // if ($value->name=='Lebanon')
-            // dd($key);
-            // $china->setLocale('zh-cn');
-            // dd(World::Countries()[76]->children());
-            // new CountryResource('Egypt');
-            // $this->country->get()
-        
-                // dd($this->country->title->get());
-                // $data['countries']= new CountryResource($this->country->get()[10]);
-            
-            $data['about-section'] = new PageResource(page('about'));
-            $data['vision-section'] = new PageResource(page('vision'));
-            $data['mission-section'] = new PageResource(page('mission'));
 
-            $data['counters'] = CounterResource::collection($this->counter->get());
+            $data['about-section'] = new PageResource(page('about'));
+
             return successResponse($data);
         } catch (Exception $e) {
 
