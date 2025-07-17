8c34739e072d1b1bc90fd992f32d948a2f9d4734
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-07-17 18:04:22 +0300

    update

--- resources/views/admin/crud/products/edit.blade.php
+++ resources/views/admin/crud/products/edit.blade.php
@@ -197,11 +197,11 @@
                                         </span>
                                     @enderror
                                 </div>
-
-                                <input type="checkbox" name="is_addition" value="1"
-                                    {{ old('is_addition', $product->is_addition) ? 'checked' : '' }}>
-                                <label for="is_addition">{{ __('general.is_addition') }}</label>
-
+                                <div class="col-md-6">
+                                    <input type="checkbox" name="is_addition" value="1"
+                                        {{ old('is_addition', $product->is_addition) ? 'checked' : '' }}>
+                                    <label for="is_addition">{{ __('general.is_addition') }}</label>
+                                </div>
     
                                    {{-- images --}}
                                 <div class="col-md-6">
