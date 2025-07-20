31720f1deddfce53351eb58a99b327fbb82a17b7
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-07-21 00:11:28 +0300

    update

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -53,7 +53,16 @@ class ProductController extends Controller
     public function show($id)
     {
         try {
-            $data['product'] = new ProductResource($this->product->findorfail($id));
+            $product=$this->product->findorfail($id);
+            $data['product'] = new ProductResource($product);
+            
+            $relatedProducts = $this->product
+            ->where('category_id', $product->category_id)
+            ->where('id', '!=', $product->id)
+            ->limit(10)
+            ->get();
+
+        $data['related_products'] = ProductResource::collection($relatedProducts);
             return successResponse($data);
         } catch (Exception $e) {
 
