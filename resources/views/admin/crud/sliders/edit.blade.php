b197adbd3deac9e4531347c3a85ef6c931930929
Boula Nessim <boula.nessim.soliman@gmail.com>
2025-09-28 15:23:45 +0300

    commit

--- resources/views/admin/crud/sliders/edit.blade.php
+++ resources/views/admin/crud/sliders/edit.blade.php
@@ -4,20 +4,73 @@
 @section('fields_content')
     <div class="content-wrapper">
         @method('PUT')
-
-        <div class="container p-3">
+        
+                <div class="container p-3">
             <div class="card card-custom mb-2">
                 <div class="card-header card-header-tabs-line">
                     @include('admin.components.breadcrumb', ['module' => 'sliders', 'action' => 'edit'])
                 </div>
-
-                <div class="card-body bg-white">
-                    <div class="digital-add needs-validation ">
-                        <div class="row">
-
-
-                            <div class="col-md-6">
+                <div class="card-toolbar px-3">
+                    <ul class="nav nav-tabs nav-bold nav-tabs-line">
+                        @foreach (config('translatable.locales') as $key => $locale)
+                            <li class="nav-item">
+                                <a class="nav-link  @if ($key == 0) active @endif" data-toggle="tab"
+                                    href="{{ '#' . $locale }}">@lang('general.' . $locale)</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <div class="tab-content">
+                        @foreach (config('translatable.locales') as $key => $locale)
+                            <div class="tab-pane fade show @if ($key == 0) active @endif"
+                                id="{{ $locale }}" role="tabpanel">
                                 <div class="form-group">
+                                    <label>{{__('general.title')}}- @lang('general.' . $locale)<span class="text-danger"> * </span></label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fas fa-pen"></i></span>
+                                        </div>
+                                        <input type="text" name="{{ $locale . '[title]' }}"
+                                            placeholder="{{__('general.title')}}"
+                                            class="form-control @error('') invalid @enderror  pl-1 min-h-40px @error($locale . '.title') is-invalid @enderror"
+                                            value="{{ old($locale . '.title', $slider->translate($locale)->title) }}">
+                                    </div>
+                                </div>
+                                @error($locale . '.title')
+                                    <span class="text-danger" >
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+
+
+                                <div class="col-form-group ">
+                                    <label>{{ __('general.description') }}(@lang('general.' . $locale))<span
+                                            class="text-danger"></span></label>
+                                    <textarea rows="5" class="summernote bg-light @error($locale . '.description') is-invalid @enderror"
+                                        name="{{ $locale . '[description]' }}">
+                                        {!! old($locale . '.description', $slider->translate($locale)->description) !!} 
+                                    </textarea>
+                                    @error($locale . '.description')
+                                        <span class="text-danger" >
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="card card-custom">
+                <div class="card-body mb-5">
+                    <div class="row">
+
+                       
+                                   {{-- images --}}
+                                   <div class="col-md-6">
                                     @include('admin.components.image', [
                                         'label' => __('general.image'),
                                         'value' => old('image', $slider->image),
@@ -27,22 +80,21 @@
                                         'required' => true,
                                     ])
 
+                                    @error('image')
+                                        <span class="text-danger" >
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+    
                                 </div>
-                                @error('image')
-                                    <span class="text-danger" >
-                                        {{ $message }}
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    
+                                @include('admin.components.images', ['images' => $images])
+    
                     </div>
                 </div>
-            </div>
-            <div class="card card-custom">
                 <div class="card-footer mb-5 mt-5">
-                    <button type="submit" class="btn btn-outline-success">{{ __('general.save') }}</button>
-                    <a href="{{ route('sliders.index') }}"
-                        class="btn btn-outline-danger font-weight-bold">{{ __('general.cancel') }}</a>
+                    <button type="submit" class="btn btn-outline-success">{{__('general.save')}}</button>
+                    <a href="{{ route('sliders.index') }}" class="btn btn-outline-danger font-weight-bold">{{__('general.cancel')}}</a>
                 </div>
             </div>
         </div>
--- resources/views/admin/crud/sliders/index.blade.php
+++ resources/views/admin/crud/sliders/index.blade.php
@@ -1,60 +1,64 @@
 @extends('admin.layouts.master')
 
 @section('content')
-    <div class="page-body">
+<div class="page-body" >
 
-        @include('admin.components.breadcrumb', [
-            'module' => 'sliders',
-            'action' => 'index',
-        ])
+    @include('admin.components.breadcrumb', [
+    'module' => 'sliders', 
+    'action' => 'index',     
+    ])
 
-        <!-- Container-fluid starts-->
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="card">
-                        <div class="card-header  d-flex justify-content-end">
-                            <a href="{{ route('sliders.create') }}"
-                                class="btn btn-primary mt-md-0 mt-2">{{ __('general.add') }}</a>
-                        </div>
+    <!-- Container-fluid starts-->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header  d-flex justify-content-end">
+                        <a href="{{route('sliders.create')}}" class="btn btn-primary mt-md-0 mt-2">{{ __('general.add') }}</a>
+                    </div>
 
-                        <div class="card-body">
-                            <div class="table-responsive table-desi">
-                                <table class="table list-digital all-package table-category " id="editableTable">
-
-                                    <thead>
+                    <div class="card-body">
+                        <div class="table-responsive table-desi">
+                            <table class="table list-digital all-package table-category " id="editableTable">
+                                
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>{{__('general.image')}}</th>
+                                        <th>{{__('general.title')}}</th>
+                                        <th>{{__('general.controls')}}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($sliders as $slider)
                                         <tr>
-                                            <th>#</th>
-                                            <th>{{ __('general.image') }}</th>
-                                            <th>{{ __('general.controls') }}</th>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td><img src="{{ $slider->image }}"
+                                                alt="{{ $slider->title }}"></td>
+                                            <td>{{ $slider->title }}</td>              
+                                   
+                                            <td>
+                                                @include('admin.components.controls', [
+                                                    'route' => 'sliders',
+                                                    'role' => 'slider',
+                                                    'module' => $slider,
+                                                ])
+                                            </td>
                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($sliders as $slider)
-                                            <tr>
-                                                <td>{{ $loop->iteration }}</td>
-                                                <td><img src="{{ $slider->image }}" alt="slider"></td>
-                                                <td>
-                                                    @include('admin.components.controls', [
-                                                        'route' => 'sliders',
-                                                        'role' => 'slider',
-                                                        'module' => $slider,
-                                                    ])
-                                                </td>
-                                            </tr>
-                                        @endforeach
+                                    @endforeach
 
-                                    </tbody>
-                                </table>
-                            </div>
+                                </tbody>
+                            </table>
                         </div>
-                        <div class="card-footer d-flex justify-content-end">
-                            {!! $sliders->links('admin.components.paginate') !!}
-                        </div>
+                    </div>
+                    <div class="card-footer d-flex justify-content-end">
+                        {!! $sliders->links('admin.components.paginate') !!}
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Container-fluid Ends-->
     </div>
+    <!-- Container-fluid Ends-->
+</div>
 @endsection
+
