6b5d9b6bd605ff4eb9fe6f7f36a2b922d8395772
Boula Nessim <boula.nessim.soliman@gmail.com>
2025-11-03 12:42:08 +0200

    commit

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
 'paths' => ['api/*', 'auth/*'],
 'allowed_origins' => ['http://localhost:5173'],
 'allowed_methods' => ['*'],
-'allowed_headers' => ['*'],
+// 'allowed_headers' => ['*'],
 
 
 ];
