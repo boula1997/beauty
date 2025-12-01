8f7934fbd37e34efc5ddb78c7925cee351d694d6
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-12-01 22:36:07 +0200

    update

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -26,7 +26,7 @@ class ProductResource extends JsonResource
             "byOneGetOne" => $this->byOneGetOne,
             "discount" => $this->discount,
             
-            "applyOffer" => $this->discount > 0 ? $this->discount .'% off'
+            "applyOffer" => $this->discount > 0 ? $this->discount .' off'
                 : ($this->byOneGetOne
                 ? 'Buy 1 Get 1'
                 : null),
