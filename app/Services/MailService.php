4a1badf0b081c5b516ff7fa4531c018de3525356
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-12-13 14:29:23 +0200

    update

--- app/Services/MailService.php
+++ app/Services/MailService.php
@@ -25,7 +25,7 @@ class MailService
             $mail->CharSet = 'UTF-8'; // Ensure UTF-8 encoding for subject and body
 
             // Email settings
-            $mail->setFrom('info@fioriflower.site', 'Fiori Flower'); // From email and name
+            $mail->setFrom('info@melovaeg.com', 'Melova'); // From email and name
             $mail->addAddress($to, $toName); // Recipient's email and name
 
             $mail->isHTML(true); // Email format to HTML
