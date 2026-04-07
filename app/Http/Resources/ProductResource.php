f583ecc374127c29d3814fbc345e15c5c4c19351
Boula Nessim <boula.nessim.soliman@gmail.com>
2026-04-07 06:55:19 +0200

    commit

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -18,8 +18,8 @@ class ProductResource extends JsonResource
     {
         $user = $request->bearerToken() ? Auth::guard('api')->user() : null;
 
-        // Check if current route is product.show
-        $isSingleProductRoute = $request->routeIs('product.show');
+        // Check if current route is product.display
+        $isSingleProductRoute = $request->routeIs('product.display');
 
         return [
             "id" => $this->id,
